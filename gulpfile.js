1d255e3d07d8efca9e0da2736967b829b0462e4d
Andrew King <pepsiotaku@gmail.com>
2019-08-17 13:02:29 -0400

    typescript, hidesExplorerArrows

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-var gulp = require('gulp');
-var generator = require('./src/icon-generator.js');
-
-gulp.task('build', function () {
-    new generator().init();
-});
-
-gulp.task('default', ['build']);
